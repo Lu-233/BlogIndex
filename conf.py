7f66ea00549bee2228233014a2fe9c89ef5d1731
Lu <lu2github@gmail.com>
2023-12-16 14:41:04 +0800

    Update

--- conf.py
+++ conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Lu. This work is licensed under CC-BY-NC-SA 4.0'
 author = 'Lu Wang'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1'
+release = '0.2'
 
 
 # -- General configuration ---------------------------------------------------
